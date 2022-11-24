97e62db31234e2daa14b88797e5998267b634c95
Tai <Tai.Delisle@telus.com>
2022-11-24 12:50:58 -0700

    update with tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,20 @@
 const request = require("request");
 
-const args = process.argv.slice(2);
+const fetchBreedDescription = (breedname, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedname}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    try {
+      callback(null, data[0].description);
+    } catch (error) {
+      let errorMsg = "Breed not found or no description available.";
+      callback(errorMsg, null);
+    }
 
-request(`https://api.thecatapi.com/v1/breads/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log(`ERR accessing API`);
-    console.log(`> ${error}`);
-    return;
-  }
-  const data = JSON.parse(body);
-  try {
-    console.log(data[0].description);
-  } catch (error) {
-    console.log(`Breed not found or no description available.`);
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
